4e61e1f3fbb9f1e2f23f520e9c14870f893e45ea
victorvaz001 <victorvaz001@gmail.com>
2023-12-30 10:09:59 -0300

    ProductDTO

---
+++ src/main/java/com/devsuperior/aula/ProductDTO.java
@@ -0,0 +1,48 @@
+package com.devsuperior.aula;
+
+import com.devsuperior.aula.dto.CategoryDTO;
+import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat)); //Instanciando o ProductDTO com as categorias aninhadas
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
+    }
+}
